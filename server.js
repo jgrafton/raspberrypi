aec7286d6113af675eefc7df7c903f7150d1afcf
John Grafton <john.grafton@gmail.com>
2012-09-22 16:17:52 +0000

    exported onRequest functionality to start function and called from index.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var http = require("http");
 
-function onRequest(request, response) {
-    console.log("Request received.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log("Request received.");
+    	response.writeHead(200, {"Content-Type": "text/plain"});
+    	response.write("Hello World");
+    	response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started on port 8888.");
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log("Server has started on port 8888.");
+exports.start = start;
